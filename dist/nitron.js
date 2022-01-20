d55f56019dfea932bc76b5e53ab567c4a9ecb621
potato <84955607+devkr-potato@users.noreply.github.com>
2022-01-20 14:06:52 +0900

    Update nitron.js

--- dist/nitron.js
+++ dist/nitron.js
@@ -13,6 +13,24 @@ const nitronDOM = new NitronDOM();
 
 class Nitron {
   constructor() {};
+  renderXML(url,get) {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == XMLHttpRequest.DONE) {
+           if (xmlhttp.status == 200) {
+                document.querySelector(get).innerHTML = xmlhttp.responseText;
+           }
+           else if (xmlhttp.status == 400) {
+              alert('There was an error 400');
+           }
+           else {
+               alert('something else other than 200 was returned');
+           }
+        }
+    };
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+  };
   ClassName(classname, classnamelist){
     document.querySelector(classname).classList = classnamelist
   };
