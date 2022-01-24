8a368f91fd3948dacb28378cd746921fc6e811cb
potato <84955607+devkr-potato@users.noreply.github.com>
2022-01-24 21:31:20 +0900

    Update index.js

--- example/styles/media/index.js
+++ example/styles/media/index.js
@@ -1,15 +1,19 @@
 // use
-const info_styles = media.styles({
-    size:'width size',
-    style:'basic',
-    media:'Styles Responsive to Width'
+const const_styles = styles({
+  style:`style`,
+  media:{
+    size:'size / ex : max-width:600px',
+    style:`media style`,
+  }
 })
 
 // media styles - example
-const info_styles = media.styles({
-    size:'790px',
-    style:'font-size: 20px;',
-    media:'font-size: 15px;'
+const info_styles = styles({
+  style:`font-size: 32px;`,
+  media:{
+    size:'max-width:790px',
+    style:`font-size: 15px;`,
+  }
 })
 
 const element = `
