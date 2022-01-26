6731da748f22920e1ddc9bcb93555f90cf81be30
potato <84955607+devkr-potato@users.noreply.github.com>
2022-01-26 19:14:28 +0900

    Create type.js

---
+++ example/styles/media/type.js
@@ -0,0 +1,22 @@
+// use
+const const_styles = styles({
+  style:`style`,
+  type:{
+    hover:'',
+    active:'',
+    .
+    .
+    .
+  },
+  media:{
+    size:'size / ex : max-width:600px',
+    style:`media style`,
+    type:{
+      hover:'',
+      active:'',
+      .
+      .
+      .
+    }
+  }
+});
