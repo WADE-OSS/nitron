b9cfc38509927cb37b31f8d969a404a10f245b89
potato <84955607+devkr-potato@users.noreply.github.com>
2022-02-23 18:22:12 +0900

    Update nitron.js

--- dist/nitron.js
+++ dist/nitron.js
@@ -1,74 +1,119 @@
-/*!
- * Nitron.js v0.0.4
- * (c) 2022 WADE Open Source Software and Nitron Team.
+/*
+ * Nitron.js v0.0.6 - dev (v1.0.0-alpha.1)
+ *
+ * (c) 2022 WADE Open Source Software and Nitron Team. and its affiliates.
  * Released under the MIT License.
+ * https://github.com/WADE-OSS/nitron/blob/main/LICENSE
  */
+
+// Error Event : #4
+window.addEventListener("error",(err)=>{
+  document.body.innerHTML = `
+    <h1 style="color:red;">${err.error}</h1>
+    <p>info : ${err.filename} | ${err.lineno}</p>
+  `;
+});
+
 class NitronDOM {
-    constructor() {};
-    render(code, queryinsertion) {
-      queryinsertion.innerHTML = code;
-    };
-      renderXML(url,get) {
-      var xmlhttp = new XMLHttpRequest();
-      xmlhttp.onreadystatechange = function() {
-          if (xmlhttp.readyState == XMLHttpRequest.DONE) {
-             if (xmlhttp.status == 200) {
-                  document.querySelector(get).innerHTML = xmlhttp.responseText;
-             }
-             else if (xmlhttp.status == 400) {
-                alert('There was an error 400');
-             }
-             else {
-                 alert('something else other than 200 was returned');
-             }
-          }
+  constructor() {};
+  // Returns an element written in JS as HTML
+  render(element, queryinsertion) {
+    element = nitron.returnDOM(element);
+
+    const xhr = new XMLHttpRequest();
+
+    queryinsertion.addEventListener("change", (event) => {
+      if(event.target.getAttribute('value-in')){
+        eval(`${event.target.getAttribute('value-in')} = "${event.target.value}"`)
       };
-      xmlhttp.open("GET", url, true);
-      xmlhttp.send();
-    };
-}
+    });
+
+    queryinsertion.addEventListener("input", (event) => {
+      if(event.target.getAttribute('value-in')){
+        eval(`${event.target.getAttribute('value-in')} = "${event.target.value}"`)
+      };
+    });
+    
+    queryinsertion.innerHTML = element;
+  };
+};
+
+// Convert NitronDOM class to const : You can use nitronDOM without declaring variables or constants. 
 const nitronDOM = new NitronDOM();
 
 class Nitron {
   constructor() {};
+
+  // Replace the Nitron syntax with HTML.
+  returnDOM(HTML){
+
+    if(HTML.match(/<[A-Z].* ?\/>/g)){
+
+      HTML.match(/<[A-Z].* ?\/>/g).forEach((doc)=>{
+        HTML = HTML.replace(doc,`<${doc.slice(1,doc.length-2)}></${doc.match(/[A-Z][a-z]*/g)}>`);
+      });
+
+    };
+
+    if(HTML.match(/<[A-Z]/g)){
+      HTML.match(/<[A-Z]/g).forEach((doc)=>{
+        HTML = HTML.replace(doc,`<dom-${doc[1]}`)
+      });
+      if(HTML.match(/\<\/[A-Z]/g)){
+        HTML.match(/\<\/[A-Z]/g).forEach((doc)=>{
+          HTML = HTML.replace(doc,`</dom-${doc[2]}`)
+        });
+      };
+    };
+
+    if(HTML.match(/<>/g)){
+      HTML.match(/<>/g).forEach((doc)=>{
+        HTML = HTML.replace(doc,`<div>`)
+      });
+      if(HTML.match(/<\/>/g)){
+        HTML.match(/<\/>/g).forEach((doc)=>{
+          HTML = HTML.replace(doc,`</div>`)
+        });
+      };
+    };
+    HTML = HTML.replace(/\s+</g,"<")
+    HTML = HTML.replace(/>\s+</g,"><")
+    return HTML;
+  };
+
+  // Create a component
   component(elementName, ComponentOptions){
-    customElements.define(`${elementName}`, class extends HTMLElement {
+    let componentName = "";
+    if(elementName.includes('-')){
+      componentName = elementName
+    }else{
+      componentName = `dom-${elementName}`
+    };
+    customElements.define(componentName, class extends HTMLElement {
       connectedCallback() {
-        if (ComponentOptions.return) {
-          if (this.getAttributeNames()) {
-            const AttrNames = this.getAttributeNames();
-            var optionsreturn = ComponentOptions.return;
-            AttrNames.forEach(attr => {
-              let val = this.getAttribute(attr);
-              optionsreturn = optionsreturn.replace(new RegExp(`\{\{ ?${attr} ?\}\}`,"g"), val);
-            });
-            this.outerHTML = optionsreturn;
-          } else {
-            this.outerHTML = ComponentOptions.return
-          }
-        }
-      }
+        let componentTemplate = nitron.returnDOM(ComponentOptions.template);
+        if(this.getAttributeNames()) {
+          const AttrNames = this.getAttributeNames();
+          AttrNames.forEach(attr => {
+            let val = this.getAttribute(attr);
+            componentTemplate = componentTemplate.replace(new RegExp(`\{\{ ?${attr} ?\}\}`,"g"),val);
+          });
+        };
+  
+        if(ComponentOptions.props){
+          Object.keys(ComponentOptions.props).forEach(x => {
+            componentTemplate = componentTemplate.replace(new RegExp(`\{\{ ?${x} ?\}\}`,"g"), ComponentOptions.props[x]);
+          });
+        };
+        this.outerHTML = componentTemplate;
+      };
     });
   };
-  ClassName(classname, classnamelist){ 
-    document.querySelector(classname).classList = classnamelist
-  };
-  randomClassName() {
-    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz'
-    const stringLength = 8
-    let randomstring = ''
-    for (let i = 0; i < stringLength; i++) {
-      const rnum = Math.floor(Math.random() * chars.length)
-      randomstring += chars.substring(rnum, rnum + 1)
-    }
-    return randomstring
-  };
-  style(selector, style) {
+  //  Element creation, Style creation API (#10) - Style creation
+  styles(selector, style){
     if (!document.styleSheets) return;
     if (document.getElementsByTagName('head').length == 0) return;
-  
     var styleSheet,mediaType;
-  
     if (document.styleSheets.length > 0) {
       for (var i = 0, l = document.styleSheets.length; i < l; i++) {
         if (document.styleSheets[i].disabled)
@@ -86,7 +131,6 @@ class Nitron {
             styleSheet = document.styleSheets[i];
           }
         }
-  
         if (typeof styleSheet !== 'undefined')
           break;
       }
@@ -101,10 +145,8 @@ class Nitron {
         }
         styleSheet = document.styleSheets[i];
       }
-  
       mediaType = typeof styleSheet.media;
     }
-  
     if (mediaType === 'string') {
       for (var i = 0, l = styleSheet.rules.length; i < l; i++) {
         if(styleSheet.rules[i].selectorText && styleSheet.rules[i].selectorText.toLowerCase()==selector.toLowerCase()) {
@@ -125,56 +167,30 @@ class Nitron {
       styleSheet.insertRule(selector + '{' + style + '}', styleSheetLength);
     }
   };
-};
-
-function styles(style="") {
-  const name = nitron.randomClassName()
-  nitron.style(`.${name}`,style)
-  if(style.style){
-    nitron.style(`.${name}`,style.style)
+  //  Element creation, Style creation API (#10) - Element creation
+  createElement(element, props,innerHTML=""){
+    if(props == null || undefined){
+      return `<${element}>${innerHTML}</${element}>`
+    }else{
+      let Attribute = "";
+      Object.keys(props).forEach((element) => {
+        Attribute += `${element}="${props[element]}"`
+      });
+      return `<${element} ${Attribute}>${innerHTML}</${element}>`
+    };
   };
-  if(style.type){
-    Object.keys(style.type).forEach(x =>
-      nitron.style(`.${name}:${x}`,style.type[x])
-    )
-  };
-  if(style.media){
-    var mediaquery = window.matchMedia(`screen and (${style.media.size})`);
-
-    if (mediaquery.matches) {
-      nitron.style(`.${name}`,style.media.style);
-      if(style.media.type){
-        Object.keys(style.media.type).forEach(x =>
-          nitron.style(`.${name}:${x}`,style.media.type[x])
-        )
-      };
-    }
-
-    mediaquery.addListener((a) => {
-      if(a.matches === true){
-         nitron.style(`.${name}`,style.media.style)
-        if(style.media.type){
-          Object.keys(style.media.type).forEach(x =>
-            nitron.style(`.${name}:${x}`,style.media.type[x])
-          )
-        }
-      }else{
-        if(style.style){
-          nitron.style(`.${name}`,style.style)
-          if(style.type){
-            Object.keys(style.type).forEach(x =>
-              nitron.style(`.${name}:${x}`,style.type[x])
-            )
-          };
-        };
-      }
-    });
-  };
-  return name
 };
 
 const nitron = new Nitron();
-  
+
+// Template
+/*
+  var data = {title:"Nitron.js"}
+  <Template data="data">
+    <h1>{{title}}</h1>
+  </Template>
+*/
+
 var placeholders = function (template, data) {'use strict';
   template = typeof (template) === 'function' ? template() : template;
   if (['string', 'number'].indexOf(typeof template) === -1) throw 'NitronDOM : please provide a valid template!';
@@ -203,3 +219,28 @@ var placeholders = function (template, data) {'use strict';
   });
   return template;
 };
+
+
+customElements.define('dom-template', class extends HTMLElement {
+  DOM(){
+    let Template_data = this.getAttribute('data');
+    Template_data = eval(Template_data);
+    let HTML = '<span style="color: red;">unknown error</span>';
+
+    if(this.getAttribute('data')){
+      if(Array.isArray(Template_data)){
+        Template_data.forEach(element => {
+          HTML += placeholders(this.innerHTML,element);
+        });
+      }else{
+        HTML = placeholders(this.innerHTML,Template_data);
+      };
+    }else{
+      HTML = this.innerHTML;
+    };
+    this.outerHTML = HTML;
+  }
+  connectedCallback() {
+    this.DOM()
+  };
+});
