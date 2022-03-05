c9ddbf365b72aa8e5dc70426b1fc7fc4e653cb2e
potato <84955607+devkr-potato@users.noreply.github.com>
2022-03-05 22:22:49 +0900

    Update nitron.bundle.js

--- dist/nitron.bundle.js
+++ dist/nitron.bundle.js
@@ -1,5 +1,5 @@
 /*
- * Nitron.js v0.0.7 - dev (v1.0.0-alpha.1)
+ * Nitron.js v0.0.8 - dev (v1.0.0-alpha.1)
  *
  * (c) 2022 WADE Open Source Software and Nitron Team. and its affiliates.
  * Released under the MIT License.
@@ -323,48 +323,51 @@ window.addEventListener('load',() => {
           this.outerHTML = `<div class="${routerRenderClassName}"></div>`;
         };
       };
-    });
-  
-    const routerLinkClassNameChars = 'ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz';
-    var routerLinkClassName = "";
-    for (let i = 0; i < 8; i++) {
-      const rnum = Math.floor(Math.random() * routerLinkClassNameChars.length)
-      routerLinkClassName += routerLinkClassNameChars.substring(rnum, rnum + 1)
-    };
-  
-    customElements.define('dom-link', class extends HTMLElement {
-      connectedCallback() {
-        let props = {};
-        Object.assign(props,{class:routerLinkClassName});
-        if(this.getAttributeNames()) {  
-          const AttrNames = this.getAttributeNames();
-          AttrNames.forEach(attr => { 
-            if(attr == "to"){
-              Object.assign(props,{href:this.getAttribute(attr)});
-            }else{
-              if(props[attr]){
-                props[attr] += ` ${this.getAttribute(attr)}`;
-              }else{
-                props[attr] = `${this.getAttribute(attr)}`;
-              };
-            }
-          }); 
-        };
-        this.outerHTML = nitron.createElement('a',props,this.innerHTML);
-      };
-    });
-  
-    const render = path => {
-      document.querySelector(`.${routerRenderClassName}`).innerHTML = routes[path] || routes["/404"];
-      document.querySelectorAll(`.${routerLinkClassName}[href^="/"]`).forEach(el => 
-        el.addEventListener("click", evt => {
-          evt.preventDefault();
-          const {pathname: path} = new URL(evt.target.href);
-          window.history.pushState({path}, path, path);
-          render(path);
-        })
-      );
-    };
-  
-    render(window.location.pathname);
   });
+  
+  const routerLinkClassNameChars = 'ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz';
+  var routerLinkClassName = "";
+  for (let i = 0; i < 8; i++) {
+    const rnum = Math.floor(Math.random() * routerLinkClassNameChars.length)
+    routerLinkClassName += routerLinkClassNameChars.substring(rnum, rnum + 1)
+  };
+
+  customElements.define('dom-link', class extends HTMLElement {
+    connectedCallback() {
+      let props = {};
+      Object.assign(props,{class:routerLinkClassName});
+      if(this.getAttributeNames()) {  
+        const AttrNames = this.getAttributeNames();
+        AttrNames.forEach(attr => { 
+          if(attr == "to"){
+            Object.assign(props,{href:this.getAttribute(attr)});
+          }else{
+            if(props[attr]){
+              props[attr] += ` ${this.getAttribute(attr)}`;
+            }else{
+              props[attr] = `${this.getAttribute(attr)}`;
+            };
+          }
+        }); 
+      };
+      this.outerHTML = nitron.createElement('a',props,this.innerHTML);
+    };
+  });
+
+  const render = path => {
+    document.querySelector(`.${routerRenderClassName}`).innerHTML = routes[path] || routes["/404"];
+    document.querySelectorAll(`.${routerLinkClassName}[href^="/"]`).forEach(el => 
+      el.addEventListener("click", evt => {
+        evt.preventDefault();
+        const {pathname: path} = new URL(evt.target.href);
+        window.history.pushState({path}, path, path);
+        render(path);
+      })
+    );
+  };
+
+  render(window.location.pathname);
+  window.addEventListener("popstate", e =>
+    render(new URL(window.location.href).pathname)
+  );
+});
